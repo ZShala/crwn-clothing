e0a9ecd5fe8cb480f01c98265b77b7dc64936f63
Zana Shala <zanashala2015@gmail.com>
2020-09-24 20:53:20 +0200

    creating sign-in-and-sign-out-page and sign-in, form-input, custom-button components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.js';
 import ShopPage from './pages/shop/shop.js';
-import Header from './components/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.js';
+import Header from './components/header/header.js';
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
